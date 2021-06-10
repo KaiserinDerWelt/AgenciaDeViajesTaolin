aa8e09c2d9aea6493353c3ee9b565efdfe0852b1
Mariana Licona <m.licona@abtis.de>
2021-06-10 20:43:00 +0200

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AgenciaDeViajesTaolin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bienvenido a la agencia de viajes Taolin");
+            Console.WriteLine("Taolin Viajes,tu conexion con el mundo");
+            Console.WriteLine("Elige un destino");
+            Console.WriteLine("Indica el numero de viajeros");
+            Console.WriteLine("Destino: ");
+            Console.WriteLine("Numero de viajeros: ");
+            Console.WriteLine("Costo total viaje");
+            Console.WriteLine("Click enter para borrar los datos y empezar de nuevo");
+        }
+    }
+}
